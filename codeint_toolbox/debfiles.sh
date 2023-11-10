bc0037b67250e0003396e8fde3c3272c0382804f
Chad Brewbaker <crb002@gmail.com>
2023-11-09 21:28:00 -0600

    update

---
+++ codeint_toolbox/debfiles.sh
@@ -0,0 +1,6 @@
+wget http://ubuntu.cs.utah.edu/ubuntu/pool/main/s/strace/strace_4.26-0.2ubuntu3_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/universe/libz/libzstd/zstd_1.4.4+dfsg-3ubuntu0.1_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/c/curl/curl_7.68.0-1ubuntu2.19_amd64.deb
+wget https://github.com/ziglang/zig/releases/download/0.11.0/zig-bootstrap-0.11.0.tar.xz
+wget http://ubuntu.cs.utah.edu/ubuntu/pool/universe/z/z3/z3_4.8.7-4build1_amd64.deb
+wget https://github.com/uutils/coreutils/releases/download/0.0.21/coreutils_0.0.21_amd64.deb
